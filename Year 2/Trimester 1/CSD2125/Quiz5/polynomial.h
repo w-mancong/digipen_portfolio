8acb82928cbc10afbb62c4971c70c8c5b5fc7157
Man Cong <w.mancong@digipen.edu>
2022-11-04 23:18:14 +0800

    Quiz 5 done

--- Year 2/Trimester 1/CSD2125/Quiz5/polynomial.h
+++ Year 2/Trimester 1/CSD2125/Quiz5/polynomial.h
@@ -1,16 +1,26 @@
-// Provide file documentation header
-// Don't include any C and C++ standard library headers!!!
-// Remember, this file is incomplete and you must provide missing details/features.
-
+/*!*****************************************************************************
+\file polynomial.h
+\author Wong Man Cong
+\par DP email: w.mancong\@digipen.edu
+\par Course: HLP3
+\par Section: A
+\par Quiz 5
+\date 4-11-2022
+\brief
+Templated class that provides functionalities to declaring and calculating
+polynomials
+*******************************************************************************/
 #ifndef POLYNOMIAL_H
 #define POLYNOMIAL_H
 
-#include <iostream> // std::ostream
+#include <iostream>
 
 namespace HLP3 
 {
-    // declare class template Polynomial
-    template <typename T1, size_t N>
+    /*!*****************************************************************************
+        \brief Templated class for Polynomial
+    *******************************************************************************/
+    template <typename T1, int N>
     class Polynomial
     {
     public:
@@ -19,22 +29,74 @@ namespace HLP3
         using const_reference = value_type const &;
         using size_type = size_t;
 
+        /*!*****************************************************************************
+            \brief Default construtor: Makes a zero polynomial
+        *******************************************************************************/
         Polynomial(void);
-        ~Polynomial(void);
+
+        /*!*****************************************************************************
+            \brief Default destructor
+        *******************************************************************************/
+        ~Polynomial(void) = default;
+
+        /*!*****************************************************************************
+            \brief Copy constructor, allows for conversion between different data types
+        *******************************************************************************/
         template <typename T2>
         Polynomial(Polynomial<T2, N> const &rhs);
+
+        /*!*****************************************************************************
+            \brief Copy assignment, allows for conversion between different data types
+        *******************************************************************************/
         template <typename T2>
         Polynomial &operator=(Polynomial<T2, N> const &rhs);
+
+        /*!*****************************************************************************
+            \brief Evaluates the expression p(x) where x = a
+
+            \param [in] a: The value to be substituted with x
+        *******************************************************************************/
         value_type operator()(int a);
 
-        template <size_t M>
+        /*!*****************************************************************************
+            \brief Multiplication of two polynomials -> p(x) * q(x)
+
+            \param [in] rhs: Polynomial q(x)
+        *******************************************************************************/
+        template <int M>
         Polynomial<T1, N + M> operator*(Polynomial<T1, M> const &rhs);
 
+        /*!*****************************************************************************
+            \brief To retrieve a reference to values
+
+            \param [in] index: Index of the array
+        *******************************************************************************/
         reference operator[](size_type index);
+
+        /*!*****************************************************************************
+            \brief To retrieve a const reference to values
+
+            \param [in] index: Index of the array
+        *******************************************************************************/
         const_reference operator[](size_type index) const;
 
     private:
-        value_type *values{ nullptr };
+        value_type values[N + 1];
+
+        /*!*****************************************************************************
+            \brief Swap values internally
+
+            \param [in] rhs: To have values be swapped with this object
+        *******************************************************************************/
+        void swap(Polynomial<T1, N> &rhs);
+
+        /*!*****************************************************************************
+            \brief Power function
+
+            \param [in] base: Base value
+            \param [in] exponent: Exponent of the base
+        *******************************************************************************/
+        value_type pow(value_type base, size_type exponent);
     };
 }
 
--- Year 2/Trimester 1/CSD2125/Quiz5/polynomial.tpp
+++ Year 2/Trimester 1/CSD2125/Quiz5/polynomial.tpp
@@ -1,8 +1,21 @@
-// Provide file documentation header
-// Don't include any library headers!!!
+/*!*****************************************************************************
+\file polynomial.tpp
+\author Wong Man Cong
+\par DP email: w.mancong\@digipen.edu
+\par Course: HLP3
+\par Section: A
+\par Quiz 5
+\date 4-11-2022
+\brief
+Templated class that provides functionalities to declaring and calculating
+polynomials
+*******************************************************************************/
 
 namespace HLP3 
 {
+  /*!*****************************************************************************
+      \brief To print out the polynomial
+  *******************************************************************************/
   template< typename T, int N > 
   std::ostream& operator<<(std::ostream &out, Polynomial<T, N> const& pol) 
   {
@@ -21,55 +34,78 @@ namespace HLP3
     return out;
   }
 
-  template <typename T1, size_t N>
-  Polynomial<T1, N>::Polynomial(void) : values{ new T1[N] }
+  template <typename T1, int N>
+  Polynomial<T1, N>::Polynomial(void)
   {
-    for (size_type i{}; i < N; ++i)
-      *(values + i) = static_cast<T1>(1);
+    for (size_type i{}; i <= N; ++i)
+      *(values + i) = static_cast<T1>(0);
   }
 
-  template <typename T1, size_t N>
-  Polynomial<T1, N>::~Polynomial(void)
-  {
-    delete[] values;
-  }
-
-  template <typename T1, size_t N>
+  template <typename T1, int N>
   template <typename T2>
   Polynomial<T1, N>::Polynomial(Polynomial<T2, N> const &rhs)
   {
-
+    for (size_type i{}; i <= N; ++i)
+      *(values + i) = static_cast<T1>(rhs[i]);
   }
 
-  template <typename T1, size_t N>
+  template <typename T1, int N>
   template <typename T2>
-  Polynomial<T1, N>& Polynomial<T1, N>::operator=(Polynomial<T2, N> const &rhs)
+  Polynomial<T1, N> &Polynomial<T1, N>::operator=(Polynomial<T2, N> const &rhs)
   {
-
+    Polynomial<T1, N> res{ rhs };
+    swap(res);
+    return *this;
   }
 
-  template <typename T1, size_t N>
+  template <typename T1, int N>
   typename Polynomial<T1, N>::value_type Polynomial<T1, N>::operator()(int a)
   {
-
+    value_type res = *values;
+    for (size_type i{1}; i <= N; ++i)
+      res += *(values + i) * static_cast<value_type>(pow(a, i));
+    return res;
   }
 
-  template <typename T1, size_t N>
-  template <size_t M>
+  template <typename T1, int N>
+  template <int M>
   Polynomial<T1, N + M> Polynomial<T1, N>::operator*(Polynomial<T1, M> const &rhs)
   {
+    Polynomial<T1, N + M> res{};
 
+    for (size_type i{}; i <= N; ++i)
+    {
+      for (size_type j{}; j <= M; ++j)
+        res[i + j] += (*this)[i] * rhs[j];
+    }
+    return res;
   }
 
-  template <typename T1, size_t N>
+  template <typename T1, int N>
   typename Polynomial<T1, N>::reference Polynomial<T1, N>::operator[](size_type index)
   {
-
+    return const_cast<reference>(const_cast<Polynomial<T1, N> const &>(*this)[index]);
   }
 
-  template <typename T1, size_t N>
+  template <typename T1, int N>
   typename Polynomial<T1, N>::const_reference Polynomial<T1, N>::operator[](size_type index) const
   {
+    // error checking if index is more than N
+    return *(values + index);
+  }
 
+  template <typename T1, int N>
+  void Polynomial<T1, N>::swap(Polynomial<T1, N> &rhs)
+  {
+    std::swap(values, rhs.values);
+  }
+
+  template <typename T1, int N>
+  typename Polynomial<T1, N>::value_type Polynomial<T1, N>::pow(value_type base, size_type exponent)
+  {
+    value_type res{ static_cast<value_type>(1) };
+    for (; exponent > 0; --exponent)
+      res *= base;
+    return res;
   }
 } // end namespace HLP3
