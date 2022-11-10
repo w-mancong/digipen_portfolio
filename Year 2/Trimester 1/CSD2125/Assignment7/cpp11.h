5766f037231ba56f6117202a59f5c10315a316f6
Man Cong <w.mancong@digipen.edu>
2022-11-11 00:03:03 +0800

    done with ass 7

--- Year 2/Trimester 1/CSD2125/Assignment7/cpp11.h
+++ Year 2/Trimester 1/CSD2125/Assignment7/cpp11.h
@@ -1,9 +1,28 @@
+/*!*****************************************************************************
+\file cpp11.h
+\author Wong Man Cong
+\par DP email: w.mancong\@digipen.edu
+\par Course: HLP3
+\par Section: A
+\par Assignment 7
+\date 10-11-2022
+\brief
+This file contains variadic function template that will evaluate 2^N at compile time
+*******************************************************************************/
+
+/*!*****************************************************************************
+    \brief To pack 2^N into index_sequence
+*******************************************************************************/
 template <uint64_t Ns, uint64_t... tail>
 struct make_sequence_impl
 {
   using type = typename make_sequence_impl<Ns - 1, tail..., 1ULL << Ns>::type;
 };
 
+/*!*****************************************************************************
+    \brief Partial specialisation template for when N reaches 0, and pass the
+    packed parameters of tail into index_sequence
+*******************************************************************************/
 template <uint64_t... tail>
 struct make_sequence_impl<0, tail...>
 {
--- Year 2/Trimester 1/CSD2125/Assignment7/cpp17.h
+++ Year 2/Trimester 1/CSD2125/Assignment7/cpp17.h
@@ -1,10 +1,32 @@
+/*!*****************************************************************************
+\file cpp17.h
+\author Wong Man Cong
+\par DP email: w.mancong\@digipen.edu
+\par Course: HLP3
+\par Section: A
+\par Assignment 7
+\date 10-11-2022
+\brief
+This file contains functions that evaluates 2^N at compile time
+*******************************************************************************/
+
+/*!*****************************************************************************
+    \brief A constexpr function that will be evaulated at compile time to pack 
+    2^N into index_sequence for printing
+
+    \return Will be deduced by the compiler
+*******************************************************************************/
 template <uint64_t Ns, uint64_t... tail>
-constexpr auto make_sequence_impl()
+auto make_sequence_impl()
 {
     if constexpr (Ns == 0)
         return index_sequence<tail..., 1>{};
-    constexpr make_sequence_impl<Ns - 1, tail..., 1ULL << Ns>();
+    else
+        return make_sequence_impl<Ns - 1, tail..., 1ULL << Ns>();
 }
 
+/*!*****************************************************************************
+    \brief type definition for deducing the type for make_sequence
+*******************************************************************************/
 template <uint64_t Ns>
 using make_sequence = decltype(make_sequence_impl<Ns>());
