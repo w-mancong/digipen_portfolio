43a5dde0de02f57ababf0f171fd154f338c7b303
Man Cong <w.mancong@digipen.edu>
2024-03-25 12:57:11 +0800

    Update compile.bat

--- Year 3/Trimester 2/Parallel Programming/Assignment3/A3P2/compile.bat
+++ Year 3/Trimester 2/Parallel Programming/Assignment3/A3P2/compile.bat
@@ -1,2 +1,4 @@
-%VULKAN_SDK%\Bin\glslc.exe -o %0\..\data\shaders\glsl\computeshader\kirsch.comp.spv %0\..\data\shaders\glsl\computeshader\kirsch.comp
+%VULKAN_SDK%\Bin\glslc.exe -o %0\..\data\shaders\glsl\computeshader\applyhisto.comp.spv %0\..\data\shaders\glsl\computeshader\applyhisto.comp
+%VULKAN_SDK%\Bin\glslc.exe -o %0\..\data\shaders\glsl\computeshader\cdfscan.comp.spv    %0\..\data\shaders\glsl\computeshader\cdfscan.comp
+%VULKAN_SDK%\Bin\glslc.exe -o %0\..\data\shaders\glsl\computeshader\histogram.comp.spv  %0\..\data\shaders\glsl\computeshader\histogram.comp
 pause
