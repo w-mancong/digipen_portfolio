a8d9da66667148f2b5297dbf4da798613064d797
Man Cong <w.mancong@digipen.edu>
2024-04-07 02:04:37 +0800

    update for good luck

--- Year 3/Trimester 2/Moble and cloud programming/Cloud project/js-cloud/ProfanityFilter.js
+++ Year 3/Trimester 2/Moble and cloud programming/Cloud project/js-cloud/ProfanityFilter.js
@@ -1,43 +1,50 @@
-const fs = require('node:fs/promises');
-
-function appendText(word, shouldCensor)
+function AppendText(word, shouldCensor)
 {
     if (shouldCensor)
-    {
         return '*'.repeat(word.length) + " ";
-    }
     return word + " ";
 }
 
-async function filterText(msg)
+// Edit this function to fit with the server fit the requirement of the server
+function FilterText()
 {
-    try {
-        const reEscape = s => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');       
-        const data = await fs.readFile('profanity_words.txt', { encoding: 'utf8' });
-        const badWords = data.split('\n').map(word => word.trim());
-        const badWordsRE = new RegExp(badWords.map(reEscape).join('|'));
-        
-        var words = msg.toLowerCase().split(/\s+/);
-        var filteredText = "";
-        for (const w of words)
-        {
-            filteredText += appendText(w, w.match(badWordsRE));
-        }
-        return filteredText;
-    } catch (err) {
-        console.log(err);
-        return "";
-    }
+    const msg = document.getElementById('userInput').value;
+    var words = msg.toLowerCase().split(/\s+/);
+    var filteredText = FilterProfanity(words);
+    filteredText = MaskNRIC(filteredText.split(/\s+/));
+    document.getElementById('output').textContent = `${filteredText}`;
 }
 
-async function filterProfanity(msg)
+function MaskNRIC(words)
 {
-    try {
-        const filteredText = await filterText(msg);
-        console.log(filteredText);
-    } catch (err) {
-        console.log(err);
-    }
+    /* 
+        ^: Asserts the start of the string.
+        [STFG]: Matches any one character from the set STFG, which are the possible starting letters of an NRIC.
+        \d{7}: Matches exactly 7 digits.
+        [A-Z]: Matches any uppercase alphabet.
+        $: Asserts the end of the string.
+     */
+    var filteredText = "";
+    const nricRegex = /^[stfg]\d{7}[a-z]$/;
+    for (const w of words)
+        filteredText += AppendText(w, nricRegex.test(w));
+    return filteredText;
 }
 
-filterProfanity("");
+function FilterProfanity(words)
+{
+    const reEscape = s => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');       
+    const badWords = [
+        'fuck', 'bitch', 'fk', 'bij', 'cb', 'cheebai', 'jibai', 'knn', 'useless', 'shit', 'lanjiao', 'sohai', 
+        'pukimak', 'anjing', 'babi', 'motherfucker', 'motherfker', 'noob', 'n00b', 'stupid', 'stoopid', 'st00pid',
+        'dumb', 'asshole', 'ass', '@ss', 'b!tch', 'b!j', '@sshole', '@ssh0le', 'lj', 'dumbfk', 'lampa', 'bullshit',
+        'bullsh!t', 'bloody', 'hell', 'bodoh', 'bod0h', 'b0doh', 'b0d0h', 'penis', 'pussy', 'vagina', 'cock', 'chicken',
+        'no balls', 'nigger', 'nigg', 'nigga', 'nigg@', 'stfu', 'shut up', 'faggot', 'f@ggot', 'f@gg0t', 'fagg0t', 'cunt',
+    ];
+    const badWordsRE = new RegExp(badWords.map(reEscape).join('|'));
+
+    var filteredText = "";
+    for (const w of words)
+        filteredText += AppendText(w, w.match(badWordsRE));
+    return filteredText;
+}
